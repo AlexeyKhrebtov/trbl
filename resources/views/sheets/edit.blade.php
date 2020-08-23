65655561c1a8a3652e6da9540e89082eaaa5e3ce
Alexey <alexeymegaex@gmail.com>
2020-08-23 20:25:02 +0300

    Причесал вывод ДВ

--- resources/views/sheets/edit.blade.php
+++ resources/views/sheets/edit.blade.php
@@ -27,20 +27,23 @@
                 </form>
             </div>
         </div>
-            <div class="col-sm mt-4 mt-sm-0">
-                <form action="{{ route('sheets.destroy', $sheet) }}" method="post">
-                    @method('DELETE')
-                    @csrf
-                    <fieldset class="border p-2 bg-white">
-                        <legend  class="w-auto">
-                            <i class="fas fa-exclamation-triangle fa-lg text-danger"></i>
-                        </legend>
-                        <div class="text-center">
-                            <input type="submit" class="btn btn-danger" onclick="return confirm('Удалить ДВ со всеми данными?')" value="Удалить ДВ">
-                        </div>
-                    </fieldset>
+            <div class="row">
+                <div class="col-sm-6 mt-5 mt-sm-5">
+                    <form action="{{ route('sheets.destroy', $sheet) }}" method="post">
+                        @method('DELETE')
+                        @csrf
+                        <fieldset class="border p-2 bg-white">
+                            <legend  class="w-auto">
+                                <i class="fas fa-exclamation-triangle fa-lg text-danger"></i>
+                            </legend>
+                            <div class="text-center">
+                                <input type="submit" class="btn btn-danger" onclick="return confirm('Удалить ДВ со всеми данными?')" value="Удалить ДВ">
+                            </div>
+                        </fieldset>
 
-                </form>
+                    </form>
+                </div>
             </div>
+
     </div>
 @endsection
